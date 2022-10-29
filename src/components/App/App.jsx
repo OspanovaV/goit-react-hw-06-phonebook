c37e387a8a7fc5a35528112f48e41dc3a2574966
OspanovaVita <vitaospanova@ukr.net>
2022-10-29 17:07:11 +0300

    add start files

---
+++ src/components/App/App.jsx
@@ -0,0 +1,97 @@
+import { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
+import { ContactForm } from 'components/ContactForm/ContactForm';
+import { Filter } from 'components/Filter/Filter';
+import { ContactList } from 'components/ContactList/ContactList';
+import { Container, Section, TitleH1, TitleH2 } from './AppStyled';
+
+export function App() {
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  const [filter, setFilter] = useState('');
+
+  //Считываются данные из localStorage и записываем в State
+  useEffect(() => {
+    const contacts = localStorage.getItem('contacts');
+    const parsedСontacts = JSON.parse(contacts);
+
+    if (parsedСontacts) {
+      setContacts(parsedСontacts);
+    }
+    return;
+  }, []);
+
+//Добавляет новый контакт в localStorage
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));   
+  }, [contacts]);
+
+// Добавляет контакт в список
+  const addContact = ({ name, number }) => {
+    const normalizedFind = name.toLowerCase();
+    const findName = contacts.find(
+      contact => contact.name.toLowerCase() === normalizedFind
+    );
+    if (findName) {
+      return alert(`${name} is already in contacts.`);
+    }
+
+    const findNumber = contacts.find(
+      contact => contact.number === number
+    );
+    if (findNumber) {
+      return alert(`This phone number is already in use.`);
+    }
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    setContacts(contacts => [...contacts, newContact]);
+   
+  };
+
+  // Возвращает результат фильтра
+  const getContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  // Удаляет контакт из списка
+  const deleteContact = contactId => {
+    setContacts(contacts => 
+      contacts.filter(contact => contact.id !== contactId)
+    );
+  };
+
+  const handleFilter = e => {
+     setFilter(e.currentTarget);   
+  };
+
+  const visibleContacts = getContacts();
+
+  return (
+        <Container>
+          <Section title="Phonebook">
+            <TitleH1>Phonebook</TitleH1>
+            <ContactForm onSubmit={addContact} />
+          </Section>
+          <Section title="Contacts">
+            <TitleH2>Contacts</TitleH2>
+            <Filter value={filter} onChange={handleFilter} />
+            <ContactList
+              contacts={visibleContacts}
+              onDeleteContact={deleteContact}
+            />
+          </Section>
+        </Container>
+      );
+}
+
+ 
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+import { Form, Label, Input, Button } from './ContactFormStyled';
+
+export function ContactForm({onSubmit}) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+//обновление состояния
+  const handleChange = event => {
+    const { name, value } = event.currentTarget;
+    switch (name) {
+      case 'name':
+        return setName(value);
+      case 'number':
+        return setNumber(value);
+      default:
+        return;
+    }
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    onSubmit({name, number});
+    setName ('');
+    setNumber ('');
+    };
+  
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Label htmlFor={name}>
+        Name
+        <Input
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleChange}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </Label>
+
+      <Label htmlFor={number}>
+        Number
+        <Input
+          type="tel"
+          name="number"
+          value={number}
+          onChange={handleChange}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+      </Label>
+
+      <Button type="submit">Add contact</Button>
+    </Form>
+  );
+}
